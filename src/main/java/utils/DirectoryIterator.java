871c19803e8e55c0bc8f410845ef45cb53827628
ibu <nakamuraibuki2020se044@gmail.com>
2024-05-28 12:51:36 +0900

    Refactored DirectoryIterator.java

--- src/main/java/utils/DirectoryIterator.java
+++ src/main/java/utils/DirectoryIterator.java
@@ -28,10 +28,10 @@ public class DirectoryIterator implements FileIterator {
 			if(f.isDirectory())
 				files.addAll(listDirectory(f.getAbsolutePath()));
 			else{
-				if (f.getName().endsWith(".java")) // include all java files
+				if (f.getName().endsWith(".java")) {
+					// include all java files
 					files.add(f);	
-				if (f.getName().endsWith(".java") && (f.getAbsolutePath().contains(File.separator + "test" + File.separator) || f.getAbsolutePath().contains(File.separator + "tests" + File.separator))); // include only test code
-				if (f.getName().endsWith(".java") && !f.getAbsolutePath().contains(File.separator + "test" + File.separator) && !f.getAbsolutePath().contains(File.separator + "tests" + File.separator)); // include only production code
+				}
 
 			}
 		}
